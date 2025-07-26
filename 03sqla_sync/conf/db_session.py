59dbe34e3367ab56ed5127afb02cf9553c933599
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-07-26 14:58:14 -0300

    Primeiro commit

---
+++ 03sqla_sync/conf/db_session.py
@@ -0,0 +1,58 @@
+import sqlalchemy as sa
+
+from sqlalchemy.orm import sessionmaker
+
+from pathlib import Path
+from typing import Optional
+
+from sqlalchemy.orm import Session
+from sqlalchemy.future.engine import Engine
+
+from models.model_base import ModelBase
+
+__engine: Optional[Engine] = None
+
+def create_engine(sqlite: bool = False) -> Engine:
+    global __engine
+
+    if __engine:
+        return
+    
+    # config SQLite
+    if sqlite:
+        arquivo_db = 'db/picoles.sqlite'
+        folder = Path(arquivo_db).parent
+        folder.mkdir(parents=False, exist_ok=True)
+
+        conn_str = f'sqlite:///{arquivo_db}'
+        __engine = sa.create_engine(url=conn_str, echo=False, connect_args={'check_same_thread': False})
+    
+    # config postgresql
+    else:
+        # postgresql://usuario:senha@localhost:porta/banco
+        conn_str = 'postgresql://postgres:280103@localhost:5432/picoles'
+        __engine = sa.create_engine(url=conn_str, echo=False)
+    
+    return __engine
+
+def create_session() -> Session:
+    global __engine
+
+    if not __engine:
+        create_engine()
+
+    __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
+
+    session: Session = __session()
+
+    return session
+
+def create_tabel() -> None:
+    global __engine
+
+    if not __engine:
+        create_engine()
+
+    import models.__all_models
+    ModelBase.metadata.drop_all(__engine)
+    ModelBase.metadata.create_all(__engine)
---
+++ 03sqla_sync/create_main.py
@@ -0,0 +1,4 @@
+from conf.db_session import create_tabel
+
+if __name__ == '__main__':
+    create_tabel()
