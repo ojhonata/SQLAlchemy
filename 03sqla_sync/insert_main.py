6494c397760518cecb34ba8fd65e0915a25374a7
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-08-01 16:03:14 -0300

    Criando os insert parte 1

---
+++ 03sqla_sync/insert_main.py
@@ -0,0 +1,99 @@
+from conf.db_session import create_session
+
+from models.aditivo_nutritivo import AditivoNutritivo
+from models.sabor import Sabor
+from models.tipo_embalagem import TipoEmbalagem
+from models.tipo_picole import TipoPicole
+from models.ingrediente import Ingrediente
+from models.conservante import Conservante
+from models.revendedor import Revendedor
+
+# 1 Aditivo Nutritivo
+def insert_aditivo_nutritivo() -> None:
+    print('Cadastrando Aditivo Nutritivo')
+
+    nome: str = input('Digite o nome do Aditivo Nutritivo: ')
+    formula_quimica: str = input('Digite a fórmula química: ')
+
+    an: AditivoNutritivo = AditivoNutritivo(nome=nome, formula_quimica=formula_quimica)
+
+    with create_session() as session:
+        session.add(an)
+
+        session.commit()
+
+
+# 2 Sabor
+def insert_sador() -> None:
+    
+    nome: str = input('Digite um sabor de picole: ')
+
+    sabor: Sabor = Sabor(nome=nome)
+
+    with create_session() as session:
+        session.add(sabor)
+        session.commit()
+
+# 3 Tipo Embalagem
+def insert_tipo_embalagem() -> None:
+    nome: str = input('Digite um Tipo de Embalagem: ')
+    te: TipoEmbalagem = TipoEmbalagem(nome=nome)
+
+    with create_session() as session:
+        session.add(te)
+        session.commit()
+
+# 4 Tipo Picole
+def insert_tipo_picole() -> None:
+    nome: str = input('Digite um Tipo de Picolé: ')
+    tp: TipoPicole = TipoPicole(nome=nome)
+
+    with create_session() as session:
+        session.add(tp)
+        session.commit()
+
+# 5 Ingredientes
+def insert_ingrediente() -> None:
+    nome: str = input('Digite o Ingrediente: ')
+    ingrediente: Ingrediente = Ingrediente(nome=nome)
+
+    with create_session() as session:
+        session.add(ingrediente)
+        session.commit()
+
+# 6 conservantes
+def insert_conservantes() -> None:
+    nome: str = input('Digite o Conservante: ')
+    descricao: str = input('Digite uma descrição do conservante: ')
+
+    conservante: Conservante = Conservante(nome=nome, descricao=descricao)
+
+    with create_session() as session:
+        session.add(conservante)
+        session.commit()
+
+# 7 revendedores
+def insert_revendedor() -> Revendedor:
+    cnpj: str = input('Digite o cnpj: ')
+    razao_social: str = input('Digite a razão social: ')
+    contato: str = input('Digite o contato: ')
+
+    revendedor: Revendedor = Revendedor(cnpj=cnpj, razao_social=razao_social, contato=contato)
+
+    with create_session() as session:
+        session.add(revendedor)
+        session.commit()
+    return revendedor
+
+
+if __name__ == '__main__':
+    #insert_aditivo_nutritivo()
+    #insert_sador()
+    #insert_tipo_embalagem()
+    #insert_tipo_picole()
+    #insert_ingrediente()
+    #insert_conservantes()
+    rev = insert_revendedor()
+    print(rev)
+
+    print(f'ID: {rev.id}')
--- 03sqla_sync/models/revendedor.py
+++ 03sqla_sync/models/revendedor.py
@@ -9,9 +9,9 @@ class Revendedor(ModelBase):
 
     id: int = sa.Column(sa.BigInteger, primary_key=True, autoincrement=True)
     data_criacao: datetime = sa.Column(sa.DateTime, default=datetime.now, index=True)
-    nome: str = sa.Column(sa.String(45), unique=True, nullable=False)
+    cnpj: str = sa.Column(sa.String(45), unique=True, nullable=False)
     razao_social: str = sa.Column(sa.String(100), nullable=False)
     contato: str = sa.Column(sa.String(100), nullable=False)
 
     def __repr__(self) -> str:
-        return f'<Revendedor: {self.nome}>'
+        return f'<Revendedor: {self.cnpj}>'
